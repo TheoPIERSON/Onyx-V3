135bbc2710b26468cba227ae0923d644c8eb38ae
Theo Pierson <thheopierson@gmail.com>
2023-12-22 12:13:19 +0100

    feat: create simple calendar in angular

--- onyx-institut-frontend/src/app/appointments-screen/appointment-calendar/appointment-calendar.component.ts
+++ onyx-institut-frontend/src/app/appointments-screen/appointment-calendar/appointment-calendar.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
+import { isSameDay, isSameMonth } from 'date-fns';
 
 @Component({
   selector: 'app-appointment-calendar',
   templateUrl: './appointment-calendar.component.html',
-  styleUrls: ['./appointment-calendar.component.css']
+  styleUrls: ['./appointment-calendar.component.css'],
 })
 export class AppointmentCalendarComponent {
+  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Week;
+  CalendarView = CalendarView;
 
+  events: CalendarEvent[] = [];
+
+  activeDayIsOpen = false;
+
+  constructor() {
+    const event1 = {
+      title: 'cours',
+      start: new Date('2023-12-22T10:30'),
+      end: new Date('2023-12-22T17:30'),
+    };
+    this.events.push(event1);
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
+  }
+  eventClicked(event: any) {
+    console.log(event);
+  }
 }
