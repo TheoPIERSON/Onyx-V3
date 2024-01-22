d46910d987ae1eed7f970886236c8b92f5b59989
Theo Pierson <thheopierson@gmail.com>
2024-01-22 15:16:24 +0100

    feat: restore deleted Angular folder

---
+++ onyx-institut-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+
+import { CustomerComponent } from './customer/customer.component';
+import { AppointmentComponent } from './appointments-screen/appointment/appointment.component';
+
+export const routes: Routes = [
+  { path: 'customers', component: CustomerComponent },
+  { path: 'appointments', component: AppointmentComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
