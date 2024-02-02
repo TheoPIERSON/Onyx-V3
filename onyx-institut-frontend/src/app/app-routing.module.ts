716b4d8410b33fef38440f5247afba237b6ff734
Theo Pierson <thheopierson@gmail.com>
2024-02-02 11:34:05 +0100

    feat: stylize a bit dashboard component and add routes

--- onyx-institut-frontend/src/app/app-routing.module.ts
+++ onyx-institut-frontend/src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { provideRouter } from '@angular/router';
 import { CustomerComponent } from './customer/customer.component';
 import { AppointmentComponent } from './appointments-screen/appointment/appointment.component';
 import { PrestationsComponent } from './prestations-screen/prestations/prestations.component';
+import { DashboardComponent } from './dashboard-screen/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'customers', component: CustomerComponent },
   { path: 'appointments', component: AppointmentComponent },
   { path: 'prestations', component: PrestationsComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
